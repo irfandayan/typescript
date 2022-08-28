ece853e016aae0e8808d4b934f8e2a8a2ea3b653
irfan <engr.irfandayan@gmail.com>
2022-08-28 13:09:50 +0500

    Add typescript basic features

---
+++ features/annotations/variables.ts
@@ -0,0 +1,56 @@
+let apples = 5;
+let speed = 'fasts';
+let hasName = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// Built in objects
+let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, false, true];
+
+// Classes
+class Car {}
+let myCar1: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
+
+// Functions
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); //{x: 10, y: 20 }
+
+// 2) When we declare a variable on one line
+// and initialize it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// 3) Variable whose type can not be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
