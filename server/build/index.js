deda9c922be32e23eabe9dffb30f01f6e5460a75
irfan <engr.irfandayan@gmail.com>
2022-09-02 19:38:37 +0500

    Add login and logged out

--- server/build/index.js
+++ server/build/index.js
@@ -4,9 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cookie_session_1 = __importDefault(require("cookie-session"));
 const loginRoutes_1 = require("./routes/loginRoutes");
 const app = (0, express_1.default)();
-app.use(express_1.default.json());
+// To support JSON-encoded bodies
+// POST: {"name":"foo","color":"red"}  <-- JSON encoding
+// add a middleware (so that express can parse request.body's json)
+// Used to parse JSON bodies
+// app.use(express.json());
+// To support URL-encoded bodies
+// POST: name=foo&color=red            <-- URL encoding
+// Parse URL-encoded bodies
+app.use(express_1.default.urlencoded());
+// Note: Thbody-parser module parses the JSON, buffer, string and URL encoded data submitted using HTTP POST request.
+app.use((0, cookie_session_1.default)({ keys: ['test'] }));
 app.use(loginRoutes_1.router);
 app.listen(3000, () => {
     console.log('Listening on port 3000.');
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,22 @@
 import express, { Request, Response } from 'express';
+import cookieSession from 'cookie-session';
 import { router } from './routes/loginRoutes';
 
 const app = express();
-app.use(express.json());
+
+// To support JSON-encoded bodies
+// POST: {"name":"foo","color":"red"}  <-- JSON encoding
+// add a middleware (so that express can parse request.body's json)
+// Used to parse JSON bodies
+// app.use(express.json());
+
+// To support URL-encoded bodies
+// POST: name=foo&color=red            <-- URL encoding
+// Parse URL-encoded bodies
+app.use(express.urlencoded());
+// Note: Thbody-parser module parses the JSON, buffer, string and URL encoded data submitted using HTTP POST request.
+
+app.use(cookieSession({ keys: ['test'] }));
 
 app.use(router);
 
